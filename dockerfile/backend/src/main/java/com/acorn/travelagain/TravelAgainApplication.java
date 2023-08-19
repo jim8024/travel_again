0285836c8674c3ddff472f274af47965c5190ae0
hyeongeun <hhg7899@gamil.com>
2023-08-19 15:14:45 +0900

    Backend 초기환경 구성

---
+++ dockerfile/backend/src/main/java/com/acorn/travelagain/TravelAgainApplication.java
@@ -0,0 +1,13 @@
+package com.acorn.travelagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAgainApplication.class, args);
+    }
+
+}
---
+++ dockerfile/backend/src/test/java/com/acorn/travelagain/TravelAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acorn.travelagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
