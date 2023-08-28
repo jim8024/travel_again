80040d2144d6f14c30d0b61d042961042cf162cf
ramachell <mjh1548@gmail.com>
2023-08-29 03:39:00 +0900

    websocket 임시

--- dockerfile/backend/src/main/java/com/acorn/core/utils/ResponseUtils.java
+++ dockerfile/backend/src/main/java/com/acorn/core/utils/ResponseUtils.java
@@ -4,6 +4,7 @@ import com.acorn.core.common.dto.ResponseDTO;
 import com.acorn.core.common.constant.CommonConstant;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.sql.Timestamp;
 
@@ -24,6 +25,23 @@ public class ResponseUtils {
             .build();
 
     return ResponseEntity.status(HttpStatus.OK.value()).body(responseDTO);//
+
+  }
+
+  public static DeferredResult<ResponseEntity<?>> errorDeferredResult(Exception e ) {
+    DeferredResult<ResponseEntity<?>> deferredResult = new DeferredResult<>();
+
+    ResponseDTO responseDTO = ResponseDTO.builder()
+            .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+            .message(e.getMessage())
+            .error("Exception")
+            .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+            .path(RequestUtils.getHttpServletRequest().getRequestURL().toString())
+            .timestamp(String.valueOf(new Timestamp(System.currentTimeMillis())))
+            .build();
+
+    deferredResult .setResult(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(responseDTO));
+    return deferredResult ;
 
   }
 
