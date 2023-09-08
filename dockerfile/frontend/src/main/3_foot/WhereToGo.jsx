7485b0c6ade32fc85db5020541a012b53eb14043
ramachell <mjh1548@gmail.com>
2023-09-08 19:39:41 +0900

    wheretogo axios make

--- dockerfile/frontend/src/main/3_foot/WhereToGo.jsx
+++ dockerfile/frontend/src/main/3_foot/WhereToGo.jsx
@@ -6,14 +6,35 @@ import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
 import IconButton from '@mui/material/IconButton';
 import NativeSelect from '@mui/material/NativeSelect';
+import axios from 'axios';
 
 function WhereToGo() {
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        // ------------ 나중에 서버랑 연결할 때 주석제거하기 ---------------------------
+        const data = new FormData(event.currentTarget);
+        const category = data.get('category')
+        const keyword = data.get('keyword')
+        const url = 'http://localhost:9000/tourlist/es/overview?searchValue=' + keyword 
+
+        try {
+            const response = await axios.post(url, {
+                
+                
+            }); 
+            console.log('서버 응답:', response.data);
+        } catch (error) {
+            console.error('오류:', error);
+        }
+    };
     return (
         <div className="container">
             <h2>어디로 여행을 떠나시나요?</h2>
             <div style={{ margin: 20, padding: 20 }}>
                 <Box
                     component="form"
+                    onSubmit={handleSubmit}
                     sx={{
                         '& .MuiTextField-root': { m: 1, width: '80ch' },
                     }}
@@ -35,7 +56,7 @@ function WhereToGo() {
                             <option value={'daejeon'}>대전</option>
                         </NativeSelect>
                     </FormControl>
-                    <TextField id="standard-search" type="search" variant="standard" />
+                    <TextField name="keyword" id="standard-search" type="search" variant="standard" />
                     <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
                         <SearchIcon />
                     </IconButton>
