9e6a21bfd0968b731c18ed5580a5fd7ec1b6861c
jiyongpark <freeman7572@gmail.com>
2023-09-09 11:43:23 +0900

    modal 수정

--- dockerfile/frontend/src/plan/CreatePlanner.jsx
+++ dockerfile/frontend/src/plan/CreatePlanner.jsx
@@ -1,134 +1,147 @@
-import React, { useState } from 'react';
-import './css/CreatePlanner.css';
-import Map from './Map';
-import PlanCard from './PlanCard';
-import { Button, Grid } from '@mui/material';
-import { Link, useLocation } from 'react-router-dom';
-import DateAccordion from './date/DateAccordion';
-import DateAlert from './date/DateAlert';
-import DatePicker from './date/DatePicker';
-import axios from 'axios';
-import { format } from 'date-fns';
-import ko from 'date-fns/locale/ko';
-import { DateRange } from '@mui/icons-material';
+import React, { useState } from "react";
+import "./css/CreatePlanner.css";
+import Map from "./Map";
+import PlanCard from "./PlanCard";
+import { Button, Grid } from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
+import DateAccordion from "./date/DateAccordion";
+import DateAlert from "./date/DateAlert";
+import DatePicker from "./date/DatePicker";
+import axios from "axios";
+import { format } from "date-fns";
+import ko from "date-fns/locale/ko";
+import { DateRange } from "@mui/icons-material";
+import RealTimeList from "./tourList/RealTimeList";
+import RecommendRank from "./tourList/RecommendRank";
 
 function CreatePlanner() {
-    const [dateLength, setDateLength] = useState(0);
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    const [selectedItems, setSelectedItems] = useState([]);
-    const [startDate, setStartDate] = useState(0);
-    const [endDate, setEndDate] = useState(0);
-    const [datesArray, setDatesArray] = useState(0);
-    const location = useLocation();
-    //console.log(location);
-    const areaData = location.state ? location.state.areaData : null;
-    //날짜 시작일, 종료일 구하는 함수 => DatePicker
+  const [dateLength, setDateLength] = useState(0);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [startDate, setStartDate] = useState(0);
+  const [endDate, setEndDate] = useState(0);
+  const [datesArray, setDatesArray] = useState(0);
+  const location = useLocation();
+  //console.log(location);
+  const areaData = location.state ? location.state.areaData : null;
+  //날짜 시작일, 종료일 구하는 함수 => DatePicker
 
-    const checkingSDate = (i) => {
-        setStartDate(i);
-    };
-    const checkingEDate = (i) => {
-        setEndDate(i);
-    };
+  const checkingSDate = (i) => {
+    setStartDate(i);
+  };
+  const checkingEDate = (i) => {
+    setEndDate(i);
+  };
 
-    const formattedStartDate = startDate ? format(startDate, 'yyyy-MM-dd EEEE', { locale: ko }) : '';
-    const formattedEndDate = endDate ? format(endDate, 'yyyy-MM-dd EEEE', { locale: ko }) : '';
+  const formattedStartDate = startDate
+    ? format(startDate, "yyyy-MM-dd EEEE", { locale: ko })
+    : "";
+  const formattedEndDate = endDate
+    ? format(endDate, "yyyy-MM-dd EEEE", { locale: ko })
+    : "";
 
-    const handleDateChange = (dateArray) => {
-        setDateLength(dateArray.length);
-        setDatesArray(dateArray)
-    };
+  const handleDateChange = (dateArray) => {
+    setDateLength(dateArray.length);
+    setDatesArray(dateArray);
+  };
 
-    const convertDay = (array) => {
-        let obj = [];
-        for (let i = 0; i < array.length; i++) {
-            for (let j = 0; j < array[i].length; j++) {
-                let arr = {};
-                arr.contentid = array[i][j].contentid;
-                arr.tourDay = i + 1;
-                arr.tourSeq = j + 1;
-                obj.push(arr);
-            }
-        }
-        return obj;
-    };
+  const convertDay = (array) => {
+    let obj = [];
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array[i].length; j++) {
+        let arr = {};
+        arr.contentid = array[i][j].contentid;
+        arr.tourDay = i + 1;
+        arr.tourSeq = j + 1;
+        obj.push(arr);
+      }
+    }
+    return obj;
+  };
 
-    const sendData = async () => {
-        const arr = convertDay(selectedItems)
-        try {
+  const sendData = async () => {
+    const arr = convertDay(selectedItems);
+    try {
+      const dataToSend = {
+        startDate: formattedStartDate,
+        endDate: formattedEndDate,
+        plannerTourlistDTOS: arr,
+      };
+      console.log(arr);
+      console.log(JSON.stringify(arr));
+      console.log(convertDay(selectedItems));
+      const response = await axios.post(
+        "http://localhost:9000/planner/insert",
+        dataToSend
+      );
 
-            const dataToSend = {
-                startDate: formattedStartDate,
-                endDate: formattedEndDate,
-                plannerTourlistDTOS: arr
-            };
-            console.log(arr)
-            console.log(JSON.stringify(arr))
-            console.log(convertDay(selectedItems))
-            const response = await axios.post('http://localhost:9000/planner/insert', dataToSend);
+      console.log("서버응답", response.data);
+    } catch (error) {
+      console.error("오류", error);
+    }
+  };
+  console.log(datesArray);
+  return (
+    <>
+      <div className="plan-header">
+        <p className="kor-title">{areaData.korTitle}</p>
+        <p className="eng-title">{areaData.engTitle}</p>
+      </div>
+      <div className="TestContainer">
+        <Grid container className="gridContainer">
+          <Grid item className="rightbar" xs={12} sm={2}>
+            <DatePicker
+              onDateChange={handleDateChange}
+              checkingSDate={checkingSDate}
+              checkingEDate={checkingEDate}
+              datesArray={datesArray}
+            />
+          </Grid>
+          <Grid
+            item
+            className="leftbar"
+            xs={12}
+            sm={2}
+            style={{ padding: "0" }}
+          >
+            <h3>선택된 여행지</h3>
+            <hr />
+            <DateAccordion
+              dateLength={dateLength}
+              setSelectedItems={setSelectedItems}
+              selectedItems={selectedItems}
+              setSelectedIndex={setSelectedIndex}
+            />
+          </Grid>
+          <DateAlert dateLength={dateLength} />
 
-            console.log('서버응답', response.data);
-        } catch (error) {
-            console.error('오류', error);
-        }
-    };
-    console.log(datesArray)
-    return (
-        <>
-            <div className="plan-header">
-                <p className="kor-title">{areaData.korTitle}</p>
-                <p className="eng-title">{areaData.engTitle}</p>
-            </div>
-            <div className="TestContainer">
-                <Grid container className="gridContainer">
-                    <Grid item className="leftbar" xs={12} sm={2}>
-                        <DatePicker
-                            onDateChange={handleDateChange}
-                            checkingSDate={checkingSDate}
-                            checkingEDate={checkingEDate}
-                            datesArray={datesArray}
-                        />
-                        <h3>선택된 여행지</h3>
-                        <hr />
-                        <DateAccordion
-                            dateLength={dateLength}
-                            setSelectedItems={setSelectedItems}
-                            selectedItems={selectedItems}
-                            setSelectedIndex={setSelectedIndex}
-                        />
-                        <DateAlert dateLength={dateLength} />
-                    </Grid>
-                    <Grid item className="maparea" xs={12} sm={8}>
-                        <Map selectedItems={selectedItems} areaData={areaData} />
-                    </Grid>
-                    <Grid item className="rightbar" xs={12} sm={2}>
-                        <PlanCard
-                            setSelectedItems={setSelectedItems}
-                            selectedItems={selectedItems}
-                            selectedIndex={selectedIndex}
-                            areaData={areaData}
-                        />
-                    </Grid>
-                </Grid>
-            </div>
-            <div className="planBtn">
-                <Link
-                    to={'/plan/detail'}
-                    state={{
-                        areaData: areaData,
-                        selectedItems: selectedItems,
-                        startDate: formattedStartDate,
-                        endDate: formattedEndDate,
-                        datesArray: datesArray
-                    }}
-                >
-                    <Button variant="contained" sx={{ backgroundColor: '#8181F7' }} onClick={sendData}>
-                        일정 생성하기
-                    </Button>
-                </Link>
-            </div>
-        </>
-    );
+          <Grid item className="maparea" xs={12} sm={8}>
+            <Map selectedItems={selectedItems} areaData={areaData} />
+          </Grid>
+        </Grid>
+      </div>
+      <div className="planBtn">
+        <Link
+          to={"/plan/detail"}
+          state={{
+            areaData: areaData,
+            selectedItems: selectedItems,
+            startDate: formattedStartDate,
+            endDate: formattedEndDate,
+            datesArray: datesArray,
+          }}
+        >
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "#8181F7" }}
+            onClick={sendData}
+          >
+            일정 생성하기
+          </Button>
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default CreatePlanner;
--- dockerfile/frontend/src/plan/modal/TourModal.jsx
+++ dockerfile/frontend/src/plan/modal/TourModal.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './TourModal.css';
-import PersonIcon from '@mui/icons-material/Person';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
 import CloseIcon from '@mui/icons-material/Close';
 import tourjson from '../../utils/finalresult.json';
+import { Button } from '@mui/material';
 
 const TourModal = ({ isOpen, onClose, contentid }) => {
+    const [expanded, setExpanded] = useState(false);
+    const [contentHeight, setContentHeight] = useState('auto');
+
+    useEffect(() => {
+        if (contentRef.current) {
+            const contentElement = contentRef.current;
+            // 컨텐츠 높이가 140px 이상인 경우에만 "더보기" 표시
+            if (contentElement.clientHeight < contentElement.scrollHeight) {
+                setContentHeight('100px');
+            }
+        }
+    }, [isOpen]);
+
+    const contentRef = useRef(null);
+
     if (!isOpen) return null;
     const tourData = tourjson.find((item) => item.contentid === contentid);
-    console.log(tourData);
+
+    const handleExpandToggle = () => {
+        setExpanded(!expanded);
+        setContentHeight(expanded ? '100px' : 'auto');
+    };
+
     return (
         <div className="tour-modal-overlay">
             <div className="tour-modal">
@@ -20,18 +38,39 @@ const TourModal = ({ isOpen, onClose, contentid }) => {
                 </div>
                 <div className="tour-modal-grid">
                     <div className="tour-modal-text">
-                        <h1 style={{ marginTop: '0px' }}>{tourData.title}</h1>
-                        <h3>{tourData.addr1}</h3>
-                        <p>{tourData.overview}</p>
-                        <div className="tour-modal-icon">
-                            <PersonIcon sx={{ fontSize: 45 }} />
-                            <FavoriteIcon sx={{ fontSize: 45 }} />
-                            <BookmarkIcon sx={{ fontSize: 45 }} />
+                        <div className="tour-madal-info">
+                            <h1 style={{ marginTop: '0px' }}>{tourData.title}</h1>
+                            <h3>{tourData.addr1}</h3>
+                        </div>
+                        <div className="tour-modal-image">
+                            <img src={tourData.firstimage} alt={tourData.title} />
+                        </div>
+                        <div
+                            className="tour-modal-content"
+                            style={{
+                                height: contentHeight,
+                                overflowY: expanded ? 'auto' : 'hidden',
+                            }}
+                            ref={contentRef}
+                        >
+                            <p className="tour-modal-overview">
+                                {tourData.overview}
+                                {contentHeight === '100px' && (
+                                    <strong className="more-link" onClick={handleExpandToggle}>
+                                        {expanded ? '접기' : '더보기'}
+                                    </strong>
+                                )}
+                            </p>
                         </div>
                     </div>
-                    <div className="tour-modal-image">
-                        <img src={tourData.firstimage} alt={tourData.title} />
-                    </div>
+                        <Button
+                            className="aaaaaaa"
+                            size="small"
+                            variant="contained"
+                            sx={{ mt: '3px', mb: '2px', mr: '10px'}}
+                        >
+                            추가
+                        </Button>
                 </div>
             </div>
         </div>
