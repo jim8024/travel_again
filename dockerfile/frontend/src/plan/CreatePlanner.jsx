7af2c78bc62db904bf103a8247809aedaee6697b
hyeongeunee <hhg7899@gmail.com>
2023-09-09 13:15:42 +0900

    where to go searchbar 수정

--- dockerfile/frontend/src/plan/CreatePlanner.jsx
+++ dockerfile/frontend/src/plan/CreatePlanner.jsx
@@ -14,6 +14,7 @@ import { DateRange } from "@mui/icons-material";
 import RealTimeList from "./tourList/RealTimeList";
 import RecommendRank from "./tourList/RecommendRank";
 
+
 function CreatePlanner() {
   const [dateLength, setDateLength] = useState(0);
   const [selectedIndex, setSelectedIndex] = useState(0);
