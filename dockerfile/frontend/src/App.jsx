4efb0001407446d8f12cbf4755c1d1a034b07411
hyeongeun <hhg7899@gmail.com>
2023-09-11 13:09:57 +0900

    DarkMode 기능 추가

--- dockerfile/frontend/src/App.jsx
+++ dockerfile/frontend/src/App.jsx
@@ -1,24 +1,26 @@
-import Main from "./main/1_head/Main"
-import HowTo from "./main/2_body/HowTo"
-import Carousel from "./main/2_body/Carousel";
-import WhereToGo from "./main/3_foot/WhereToGo"
-import TourListPage from "./main/3_foot/tour/TourListPage";
-import MainCarousel from "./main/2_body/MainCarousel";
-import PlanDrawer from "./plan/PlanDrawer";
-import RecommendList from "./main/2_body/RecommandList";
+import Main from './main/1_head/Main';
+import HowTo from './main/2_body/HowTo';
+import Carousel from './main/2_body/Carousel';
+import WhereToGo from './main/3_foot/WhereToGo';
+import TourListPage from './main/3_foot/tour/TourListPage';
+import MainCarousel from './main/2_body/MainCarousel';
+import PlanDrawer from './plan/PlanDrawer';
+import RecommendList from './main/2_body/RecommandList';
+import DarkModeToggleComponent from './main/1_head/DarkModeToggle';
 function App() {
-    return(
+    return (
         <>
-            <Main /> 
-            <HowTo/>
+            <DarkModeToggleComponent />
+            <Main />
+            <HowTo />
             {/* <Carousel/> */}
-            <RecommendList/>
-            <MainCarousel/>
-            <WhereToGo/>
-            <TourListPage/>  
-            <PlanDrawer/>
+            <RecommendList />
+            <MainCarousel />
+            <WhereToGo />
+            <TourListPage />
+            <PlanDrawer />
         </>
-    )
+    );
 }
 
 export default App;
