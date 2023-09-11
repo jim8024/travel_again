319dbd8ba1e63f229e121aef4952622ef79e50b2
jiyongpark <freeman7572@gmail.com>
2023-09-11 09:41:37 +0900

    디테일 지도 페이지 구현

--- dockerfile/frontend/src/PlanDetail/tabs_item/BigMap.jsx
+++ dockerfile/frontend/src/PlanDetail/tabs_item/BigMap.jsx
@@ -4,16 +4,33 @@ import { IconButton } from "@mui/material";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import BigMapItem from "../asset/BigMapItem";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
-export default function BigMap({ selectedItems, areaData, day }){
+export default function BigMap(){
+  const location = useLocation();
+  const areaData = location.state ? location.state.areaData : null;
+  const selectedItems = location.state ? location.state.selectedItems : null;
+  
+  const [day, setDay] = useState(0);
+
+  const itemLength = selectedItems ? selectedItems.length : 0;
+  
+  const handleDayNext = () => {
+    setDay((prevDay) => Math.min(prevDay + 1, itemLength - 1));
+  };
+
+  const handleDayPrev = () => {
+    setDay((prevDay) => Math.max(prevDay - 1, 0));
+  };
     return(
         <>
           <div className="map-header">
-            <IconButton sx={{}} className="backBtn">
+            <IconButton sx={{}} className="backBtn" onClick={handleDayPrev}>
               <ArrowBackIosIcon sx={{ color: "#EEEEEE" }} />
             </IconButton>
-            <p className="day_input" style={{margin:"0"}}>DAY 1</p>
-            <IconButton sx={{}} className="forwardBtn">
+            <p className="day_input" style={{margin:"0"}}>DAY{day+1}</p>
+            <IconButton sx={{}} className="forwardBtn"onClick={handleDayNext}>
               <ArrowForwardIosIcon sx={{ color: "#EEEEEE" }} />
             </IconButton>
         </div>
