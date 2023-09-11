1116d151cca3dd14ff2181d1b8bebf8d27a41264
jiyongpark <freeman7572@gmail.com>
2023-09-11 15:12:33 +0900

    메인 검색 추가

--- dockerfile/frontend/src/App.jsx
+++ dockerfile/frontend/src/App.jsx
@@ -16,7 +16,6 @@ function App() {
             {/* <Carousel/> */}
             <RecommendList />
             <MainCarousel />
-            <WhereToGo />
             <TourListPage />
             <PlanDrawer />
         </>
